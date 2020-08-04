b11e3d6f01cbe2fad01ae190dc82016d10aeea92
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2020-08-04 17:32:46 +0200

    updated settings for database

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pur_beurre',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
@@ -104,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
