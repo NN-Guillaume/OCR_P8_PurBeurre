10c6e8f2e78e84746ed350ff4178cb0ebcfaebb8
NN-Guillaume <35535667+NN-Guillaume@users.noreply.github.com>
2020-09-28 21:30:07 +0200

    Add files via upload

--- pur_beurre/rechercher/models.py
+++ pur_beurre/rechercher/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Categories(models.Model):
-    id_categorie = models.IntegerField('id_categorie', null=True) #PK
+    id_categorie = models.IntegerField('id_categorie', null=False) #PK
     categorie_name = models.CharField('categorie_name', max_length=200, unique=True)
 
 class Products(models.Model):
@@ -14,7 +14,6 @@ class Products(models.Model):
     img_product= models.URLField('img_product',)
     where_to_buy = models.CharField('where_to_buy', max_length=300)
     url = models.CharField('url', null=False, max_length=300, unique=True)
-
 
 class Favorites(models.Model):
     id_favorites = models.IntegerField('id_favorites', null=False) #PFK
